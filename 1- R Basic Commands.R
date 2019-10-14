b84f1105293b5e9ff68313f779c4595e91831a95
creatorHead <droidbit1@gmail.com>
2019-10-14 13:38:25 -0700

    First Commit

---
+++ 1- R Basic Commands.R
@@ -0,0 +1,24 @@
+### Assignments
+x = 1
+y <- 3 
+  #Both '=' and '<-' are same
+z = "Hello World"
+z = 'Hello World'
+  # Remember: R is case sensitive. 'z' is different than 'Z'
+
+# We cannot add non-numeric variable with numberic variable
+str = 'hello'
+num = 5
+num + str # output: Error in num + str : non-numeric argument to binary operator
+
+
+
+### Calling Functions
+print("Hello World")
+
+
+### Calculations
+1 + 2 
+x + 5
+y = x + 5
+x = sqrt(16)
---
+++ 3- Data Structures in R.R
@@ -0,0 +1,5 @@
+### Data Structures in R
+
+  # Vector: It's like list, sequence of the same data types. Elements in vector are called as Components
+
+
